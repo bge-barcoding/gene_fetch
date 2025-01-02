571cc52407fee853e6d3dd96e8515e8876457e41
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-01-02 09:51:21 +0000

    Create 1_gene_fetch.sh

---
+++ 1_gene_fetch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=GF
+#SBATCH --partition=day
+#SBATCH --output=%x.out
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=8
+#SBATCH --error=%x.err
+
+
+#activate conda env
+source ~/miniconda3/etc/profile.d/conda.sh
+conda init bash
+conda activate gene_fetch
+
+
+GENE=cox1
+
+TYPE=both
+
+OUTPUT_DIR=path/to/out/dir
+
+SAMPLES_CSV=path/to/input/samples.csv
+
+python 1_gene_fetch.py \
+	$GENE \
+	$OUTPUT_DIR \
+	$SAMPLES_CSV \
+	--type $TYPE
