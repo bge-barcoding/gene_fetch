5265044c79efd95f823f90c6abbfb343d5d0399b
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-04-16 14:32:16 +0100

    Update 1_gene_fetch.sh

--- 1_gene_fetch.sh
+++ 1_gene_fetch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=GF
 #SBATCH --partition=day
-#SBATCH --output=%x.out
-#SBATCH --mem=20G
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mem=10G
 #SBATCH --cpus-per-task=8
-#SBATCH --error=%x.err
+
 
 
 #activate conda env
@@ -12,15 +13,26 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda init bash
 conda activate fetch
 
+# NCBI API parameters
+EMAIL=user_email@domain.ac.uk
+API_KEY= XYZ
 
+# Gene search variables
 GENE=cox1
-
 TYPE=both
+
+#MIN_PROT_SIZE=
+#MIN_NUC_Size=
+#MAX_SEQS=
+
+# Input and output directory paths
+SAMPLES_CSV=path/to/input/samples.csv
+#TAXONOMY_CSV=path/ti/input/samples_taxonomy.csv
 
 OUTPUT_DIR=path/to/out/dir
 
-SAMPLES_CSV=path/to/input/samples.csv
 
+# Run gene_fetch.py
 python 1_gene_fetch.py \
 	$GENE \
 	$OUTPUT_DIR \
