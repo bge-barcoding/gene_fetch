d829e81da92e5cbe3d338a333b17d77e0c2e5fff
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-07-03 12:50:37 +0100

    Update __init__.py

--- src/gene_fetch/__init__.py
+++ src/gene_fetch/__init__.py
@@ -5,7 +5,7 @@ This package fetches sequence data from NCBI databases
 using sample taxonomic information.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.11"
 
 from .core import Config
 from .entrez_handler import EntrezHandler
