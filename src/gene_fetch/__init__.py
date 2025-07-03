a0a67b3c25fc2b1a2ef663eaab876d4644572c56
Dan Parsons <108939145+SchistoDan@users.noreply.github.com>
2025-07-03 12:50:28 +0100

    Update __init__.py

--- src/gene_fetch/__init__.py
+++ src/gene_fetch/__init__.py
@@ -11,6 +11,3 @@ from .core import Config
 from .entrez_handler import EntrezHandler
 from .sequence_processor import SequenceProcessor
 from .output_manager import OutputManager, save_genbank_file
-
-# Expose main entry point for usage
-from .main import main
