fa5b4e8b3cfe50e4518bc6c731daf5b66ae4a0f8
Daniel Parsons <dparsons@gruffalo.hpc.hutton.ac.uk>
2025-12-05 12:16:07 +0000

    v1.0.20

--- src/gene_fetch/__init__.py
+++ src/gene_fetch/__init__.py
@@ -5,7 +5,7 @@ This package fetches sequence data from NCBI databases
 using sample taxonomic information.
 """
 
-__version__ = "1.0.19"
+__version__ = "1.0.20"
 
 from .core import Config
 from .entrez_handler import EntrezHandler
